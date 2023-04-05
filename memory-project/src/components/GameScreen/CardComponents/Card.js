e7d340f48c8757573bf92f9cb4fcc518d9a4b0fa
waffleonwaffle <anavmehta1@gmail.com>
2023-04-05 01:08:54 -0700

    Changed card grid background and added hover effect to cards

--- memory-project/src/components/GameScreen/CardComponents/Card.js
+++ memory-project/src/components/GameScreen/CardComponents/Card.js
@@ -1,10 +1,10 @@
 import SVG from 'react-inlinesvg';
-const Card = ({ operator, operatorName, handleCardClick}) => {
+const Card = ({ operator, operatorName, handleCardClick }) => {
     return (
         <section className="card" onClick={handleCardClick}>
             <div>
-                <h2 className='operator-name'>{operatorName}</h2>
-                <SVG src={operator}/>
+                <h2 className='operator-name' style={{color:'white'}}>{operatorName}</h2>  
+                <SVG src={operator} />
             </div>
         </section>
     )
